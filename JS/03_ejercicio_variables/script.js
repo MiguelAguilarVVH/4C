d3f1cfcf3e5139e23245133c95d86392a13c1343
MiguelAguilarVVH <miguelaguilar@liceovvh.cl>
2025-03-11 11:03:11 -0300

    Tarea variables

---
+++ JS/03_ejercicio_variables/script.js
@@ -0,0 +1,39 @@
+console.log("Todo bien");
+
+function numerosEntero(){
+    alert(num);
+};
+
+function numeroDecimal(){
+    alert(decimal);
+};
+
+function cadenaTexto(){
+    alert(text);
+};
+
+function mostrarBoolean(){
+    alert(boolean);
+};
+
+function mostrarArray(){
+    alert("array is: \n" + array);
+};
+function mostrarMatriz(){
+    alert("Matriz is: \n" + matriz);
+};
+
+function mostrarindefinida(){
+    alert(indefinida);
+};
+
+
+
+let num = 8;
+let decimal = 3.14;
+let text = "Hola, Java Script";
+let boolean = false;
+let indefinida = undefined;
+let array = ["vate",3,2,1]
+let matriz =[[1,3],[2,4]];
+
