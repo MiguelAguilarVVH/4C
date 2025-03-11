8fdfe45d9e1c598161ac427b82819dc99a5fd62a
MiguelAguilarVVH <miguelaguilar@liceovvh.cl>
2025-03-11 11:04:07 -0300

    ok

---
+++ JS/02_tipos_variables/script.js
@@ -0,0 +1,15 @@
+const rut = 209123234587643
+let numero =0;
+let decimal = 2.5;
+let texto = "Hola";
+let booleano = true;
+
+let indefinida =  undefined;
+
+let array = [1,"Hola",25];
+let matriz = [[1,2],[3,4]];
+let objeto = {nombre: "Delay", Edad:18};
+
+function sumar(a,b){
+    return a + b
+};
